09e7c319d7ccb32822843114f7b9ef05d38a5768
Bùi Việt Thành <thanhbv@sandinh.net>
2023-06-16 13:08:33 +0700

    ramap atifacts for all configurations

--- src/main/scala/com/sandinh/javamodule/moduleinfo/ModuleInfoPlugin.scala
+++ src/main/scala/com/sandinh/javamodule/moduleinfo/ModuleInfoPlugin.scala
@@ -4,10 +4,7 @@ import sbt.*
 import sbt.Keys.*
 import Utils.*
 import ExtraJavaModuleInfoTransform.{addAutomaticModuleName, addModuleDescriptor}
-import sbt.Project.inConfig
-import sbt.librarymanagement.Configurations.Compile
-import sbt.plugins.DependencyTreeSettings
-import sbt.plugins.MiniDependencyTreeKeys.dependencyTreeIncludeScalaLibrary
+import sbt.librarymanagement.Configurations.RuntimeInternal
 import sbt.sandinh.DependencyTreeAccess
 import sbt.sandinh.DependencyTreeAccess.{moduleInfoDepGraph, toDepsMap}
 
@@ -38,52 +35,48 @@ object ModuleInfoPlugin extends AutoPlugin {
           toDepsMap((Compile / moduleInfoDepGraph).value),
           toDepsMap((Runtime / moduleInfoDepGraph).value)
         )
-        val configs = report.configurations.map {
-          case c if !interestedConfigs.contains(c.configuration) => c
-          case c =>
-            c.withModules(c.modules.flatMap { mod =>
-              val id = mod.module.jmodId
-              val willBeMerged = infos.exists(_.mergedJars.contains(id))
-              if (willBeMerged) Nil
-              else {
-                val originalJar = mod.artifacts.collect {
-                  case (a, f) if a.extension == "jar" => f
-                } match {
-                  case Vector(f) => f
-                  case arts      => sys.error(s"Cant find art for $id. $arts")
-                }
-
-                def moduleJar = out / originalJar.name
-
-                val remappedJar = infos.find(_.id == id) match {
-                  case Some(_: KnownModule) => originalJar
-                  case Some(moduleInfo: JModuleInfo) =>
-                    genIfNotExist(moduleJar, addModuleDescriptor(args, originalJar, _, moduleInfo))
-                  case Some(moduleSpec: AutomaticModuleName) =>
-                    genIfNotExist(
-                      moduleJar,
-                      addAutomaticModuleName(args.artifacts, originalJar, _, moduleSpec)
+        val remapped = report
+          .configuration(RuntimeInternal)
+          .get
+          .modules
+          .map { mod =>
+            val id = mod.module.jmodId
+            if (infos.exists(_.mergedJars.contains(id))) id -> Nil
+            else {
+              val originalJar = jarOf(mod)
+              val moduleJar = out / originalJar.name
+              val remappedJar = infos.find(_.id == id) match {
+                case Some(_: KnownModule) => originalJar
+                case Some(moduleInfo: JModuleInfo) =>
+                  genIfNotExist(moduleJar, addModuleDescriptor(args, originalJar, _, moduleInfo))
+                case Some(moduleSpec: AutomaticModuleName) =>
+                  genIfNotExist(
+                    moduleJar,
+                    addAutomaticModuleName(args.artifacts, originalJar, _, moduleSpec)
+                  )
+                case None =>
+                  if (originalJar.isModule || originalJar.isAutoModule) originalJar
+                  else if (!failOnMissing) {
+                    log.warn(
+                      s"Not a module and no mapping defined: $id -> $originalJar"
                     )
-                  case None =>
-                    if (originalJar.isModule || originalJar.isAutoModule) originalJar
-                    else if (!failOnMissing) {
-                      log.warn(
-                        s"[${c.configuration.name}] Not a module and no mapping defined: $id -> $originalJar"
-                      )
-                      originalJar
-                    } else
-                      throw new RuntimeException(
-                        s"Not a module and no mapping defined: $id -> $originalJar"
-                      )
-                }
-                mod.withArtifacts(mod.artifacts.map {
-                  case x @ (a, _) if a.extension != "jar" => x
-                  case (a, _)                             => a -> remappedJar
-                }) +: Nil
+                    originalJar
+                  } else
+                    sys.error(s"Not a module and no mapping defined: $id -> $originalJar")
               }
-            })
-        }
-        report.withConfigurations(configs)
+              id -> List(remappedJar)
+            }
+          }
+          .toMap
+        report.withConfigurations(report.configurations.map { c =>
+          c.withModules(c.modules.flatMap { mod =>
+            remapped.get(mod.module.jmodId) match {
+              case None                    => mod +: Nil
+              case Some(Nil)               => Nil
+              case Some(List(remappedJar)) => withJar(mod, remappedJar) +: Nil
+            }
+          })
+        })
       },
     )
 
@@ -91,5 +84,14 @@ object ModuleInfoPlugin extends AutoPlugin {
     if (f.isFile) f
     else { gen(f); f.ensuring(_.isFile) }
 
-  private val interestedConfigs = Seq(Compile, Runtime).map(_.toConfigRef)
+  private def jarOf(mod: ModuleReport) = mod.artifacts.collect {
+    case (a, f) if a.extension == "jar" => f
+  } match {
+    case Vector(f) => f
+    case arts      => sys.error(s"Cant find art for ${mod.module.jmodId}. $arts")
+  }
+  private def withJar(mod: ModuleReport, remappedJar: File) = mod.withArtifacts(mod.artifacts.map {
+    case x @ (a, _) if a.extension != "jar" => x
+    case (a, _)                             => a -> remappedJar
+  })
 }
